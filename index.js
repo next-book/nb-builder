f357db92f20222c54a8178045a5404bd7193a39d
Jan Martinek <honza.martinek@gmail.com>
2017-06-17 20:16:09 +0200

    Better content structuring (prototyping)

--- index.js
+++ index.js
@@ -1,102 +1,48 @@
 const fs = require('fs');
-const chapter = require('./lib/chapter');
-const bib = require('./lib/bib');
-const rimraf = require('rimraf');
+
 const yaml = require('js-yaml');
+const dot = require('dot');
+
+const persistence = require('./lib/persistence');
+const revision = require('./lib/revision');
+const bib = require('./lib/bib');
+const content = require('./lib/content');
+const style = require('./lib/style');
+const scripts = require('./lib/scripts');
 
 const build = (args) => {
-	const conf = yaml.safeLoad(fs.readFileSync(args.src + '/config.yaml', 'utf8'));
-	
-	const dirs = prepDirs(args.src, args.out, conf.dirs);
-	
-	console.log(dirs);
-	
-	const meta = {
-		revision: require('./lib/revision').getRevision(dirs.src.root),
-		date: new Date().toISOString()
-	}
-	
-	// build style
-	buildStyle(dirs.src.style, dirs.out.root);
+  const config = getConfig(args.src);
+  const dirs = persistence.prepDirs(args.src, args.out, config.dirs);
+  const meta = Object.assign({}, config.about, revision.build(dirs.src.root));
 
-	// load bibliography
-	const citations = bib.parse([fs.readFileSync(dirs.src.bibliography + '/Bibliografie.md', 'utf8')]);
+  // build style
+  style.build(dirs.src.style, `${dirs.out.root}/style`);
 
-	// build navigation
+  // build scripts
+  scripts.build(dirs.src.scripts, `${dirs.out.root}/scripts`);
+  
+  // copy assets
+  persistence.copyFolder(dirs.src.assets)('assets');
 
-	// build index
+  // load bibliography
+  const bibItems = bib.load(dirs.src.bibliography, dirs.out.root);
+  persistence.saveBibliography(bib.buildHtml(bibItems), bib.buildJson(bibItems));
 
-	// build pages
-	const filenames = getChapterFilenames(dirs.src.text);
-	buildChapters(filenames, dirs, meta);
-	
-	// write revision.json to build/ with git rev hash
-	fs.writeFile(args.out + '/rev.json', JSON.stringify( { conf, meta, args } ), (err) => {
-		if (err) throw err;
-	});
+  // build pages
+  const contents = persistence.loadContent(dirs.src.text);
+  const templates = dot.process({ path: dirs.src.templates });
+  persistence.saveFolder(dirs.out.root)(content.build(contents, templates, meta, bib.buildJson(bibItems), bib.buildHtml(bibItems)));
 
-	// write subscriber id
-	fs.writeFile(args.out + '/subscriber.json', JSON.stringify(args), (err) => {
-		if (err) throw err;
-	});
+  writeMeta(`${args.out}/meta.json`)({ config, meta, args });
 
-	// write LICENSE.md
-}
+  // write license
+};
 
+const getConfig = srcDir => yaml.safeLoad(fs.readFileSync(`${srcDir}/config.yaml`, 'utf8'));
 
+const writeMeta = path => contents => fs.writeFile(path, JSON.stringify(contents), (err) => {
+  if (err) throw err;
+});
 
-const buildStyle = (srcDir, outDir) => {
-	const sass = require('node-sass');
-	
-	const sassConfig = { 
-		file: srcDir + '/style.scss', 
-		outFile: outDir + '/style/style.css',
-		sourceMap: true
-	};
-	
-	sass.render(sassConfig, (err, result) => {
-		if (err) throw err;
-		
-		fs.mkdirSync(outDir + '/style');
-		
-		fs.writeFile(sassConfig.outFile, result.css.toString(), (err) => {
-			if (err) throw err;
-		});
-		
-		fs.writeFile(sassConfig.outFile + '.map', result.map.toString(), (err) => {
-			if (err) throw err;
-		});
-	});
-}
-
-const prepDirs = (srcDir, outDir, confDirs) => {
-	const src = Object.keys(confDirs).reduce((acc, key) => { acc[key] = srcDir + '/' + confDirs[key]; return acc; }, {});
-	src.root = srcDir;
-	console.log(src);
-	
-	const out = { root: outDir };
-
-	if (fs.existsSync(out.root)) rimraf.sync(out.root);
-	fs.mkdirSync(out.root);
-
-	return { src, out };
-}
-
-
-const getChapterFilenames = srcDir => fs.readdirSync(srcDir).filter(file => file.match('^[0-9]+-'));
-
-const buildChapters = (filenames, dirs, meta) => {
-	const templateFn = chapter.loadTemplates(dirs.src.templates).chapter;
-	
-	filenames.map(file => {
-		fs.readFile(dirs.src.text + '/' + file, 'utf8', (err, data) => {
-			if (err) throw err;
-			
-			fs.writeFile(dirs.out.root + '/' + file + '.html', chapter.build(templateFn, data, meta), (err) => {
-				if (err) throw err;
-			});
-		});
-	});
-}
 
 module.exports = build;
